1c06b1bb4a617d76d72116035db31f39d36f3124
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-09-10 22:58:39 +0500

    add info.py and write info

---
+++ info.py
@@ -0,0 +1,18 @@
+"""
+Open budgetga o'xshagan sistema yaratish kerak. Unda quyidagicha jarayonlar bo'ladi.
+
+Dastlab admin so'rovlarni qabul qilishni boshlaydi. Keyin oddiy xalq murojatlarini
+ularga yuboradi admin bazilarini qabul qiladi bazilarini bekor qiladi.
+Hamma murojatlar yig'ilgach ovoz yig'iz jarayoni boshlanadi. Har bir oddiy fuqoro
+bir ovoz berish jarayonida bitta murojatga ovoz berishi mumkin bo'ladi. Buning uchun
+telefon raqaamdan foydalaniladi. Bizda sms imkoniyati yo'q bo'lgani uchun istasangiz
+gmail dan foydalanishingiz mumkin, telefon raqam bo'lib sms siz bo'lsa ham bo'ladi.
+
+Har bir oddiy fuqoro murojatlarni umumiy natijalarini live ko'rib borish imkoniyatiga
+ega bo'lishi kerak. Ovoz berish jarayoni tugagandan keyin admin jarayonnni tugatadi va
+avtomatik g'oliblar e'lon qilinadi. Murojatlar malum bir kategoriyalarga bo'linganligi uchun
+har bir kategoriya bo'yicha nechtadandir murojat g'olib deb topiladi.
+
+Undan tashqari boshqa imkoniyatlarni vaziyatdan kelib chiqib qo'shishingiz mumkin bo'ladi.
+
+"""
