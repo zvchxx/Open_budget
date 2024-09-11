d55b334a7d50ef4c9d2a3f20e824fd9de705e6fb
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-09-11 21:00:52 +0500

    update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,3 +1,29 @@
+# from queries.for_user import get_user_from_email_query
+
+
+# def login():
+#     """
+#     Handles the login process for a user.
+#     """
+#     email: str = input("Enter your email address: ")
+#     password: str = input("Enter your password: ")
+
+#     if email == "abubakrrahmatullayev1001@gmail.com" and password == "admin":
+#         print("Login successful as Super Admin!")
+#         return "abubakrrahmatullayev1001@gmail.com"
+
+#     user_data = get_user_from_email_query(email)
+#     if user_data is None:
+#         print("Invalid email or password. Please try again.")
+#         return None
+
+#     if user_data['password'] != password:
+#         print("Invalid password. Please try again.")
+#         return None
+
+#     return user_data['email'], 'user'
+
+
 from queries.for_user import get_user_from_email_query
 
 
@@ -8,17 +34,19 @@ def login():
     email: str = input("Enter your email address: ")
     password: str = input("Enter your password: ")
 
-    if email == "abubakrrahmatullayev1001@gmail.com" and password == "admin":
-        print("Login successful as Super Admin!")
-        return "abubakrrahmatullayev1001@gmail.com"
+    if email == "admin" and password == "admin":
+        print("Login successful as Admin!")
+        return "admin", "admin"
+
 
     user_data = get_user_from_email_query(email)
     if user_data is None:
-        print("Invalid email or password. Please try again.")
+        print("Invalid email address. Please try again.")
         return None
+
 
     if user_data['password'] != password:
         print("Invalid password. Please try again.")
         return None
 
-    return user_data['email'], 'user'
+    return user_data['email'], "user"
