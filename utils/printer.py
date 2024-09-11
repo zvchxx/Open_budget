aef45c453bcd1fba2e1a409570d6e622c60018c0
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-09-11 14:33:13 +0500

    create utils, additions.py and printer.py

---
+++ utils/printer.py
@@ -0,0 +1,5 @@
+def user_printer(user):
+      print(f"""ID: {user[0]}, First name: {user[1]}, Last name: {user[2]}, 
+                  Email: {user[3]}, Password: {user[4]}, Role ID: {user[5]},
+                  Create at: {user[4]}""")
+      return None
