d51dbc6a5f7fbd2492d8db50e5f498209e00498e
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-09-13 16:33:32 +0500

    finish

--- log/logs.py
+++ log/logs.py
@@ -5,7 +5,7 @@ def log_settings():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         datefmt="%Y-%m-%d %H:%M:%S",
-                        filename="app.log")
+                        filename=".app.log")
 
     return logging.getLogger(__name__)
     
