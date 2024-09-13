36a59f71002a5f37525e7825c0051b1f247f88d1
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-09-13 13:40:31 +0500

    update admin_func.py and admin_menu.py

--- users/admin/admin_func.py
+++ users/admin/admin_func.py
@@ -1,4 +1,4 @@
-from queries.for_season import insert_season_query, update_season_query, delete_season_query     
+from queries.for_season import insert_season_query, update_season_query, delete_season_query  
 
 
 
@@ -7,10 +7,11 @@ from queries.for_season import insert_season_query, update_season_query, delete_
 
 
 def season_create():
-    start_date = "Enter season start date(YYYY-MM-DD HH:MM:SS): "
-    end_date = "Enter season end date(YYYY-MM-DD HH:MM:SS): "
+    start_date = input("Enter season start date(YYYY-MM-DD HH:MM:SS): ")
+    end_date = input("Enter season end date(YYYY-MM-DD HH:MM:SS): ")
+    money = input("Enter season money: ")
 
-    insert_season_query(start_date=start_date, end_date=end_date, is_active=True, status=True)
+    insert_season_query(start_date=start_date, end_date=end_date, is_active=True, money=money,status=True)
     print(f"\nCreated Successfully!") 
     return None
 
@@ -19,8 +20,9 @@ def season_update():
     season_id = int(input("Enter season ID: "))
     start_date = int(input("Enter season start date(YYYY-MM-DD HH:MM:SS): "))
     end_date = int(input("Enter season end date(YYYY-MM-DD HH:MM:SS): "))
+    money = input("Enter season money: ")
 
-    update_season_query(season_id=season_id, start_date=start_date, end_date=end_date, is_active=True, status=True)
+    update_season_query(season_id=season_id, start_date=start_date, end_date=end_date, is_active=True, money=money, status=True)
     print(f"Update Successfully!") 
     return None
 
@@ -35,3 +37,12 @@ def season_delete():
     
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                                            #Accepting function
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+def accepting_requests_by_id():
+    appeal_id = input("Enter appeal ID: ")
+    inf = input("Enter appeal information: ")
+
+    pass
--- users/admin/admin_menu.py
+++ users/admin/admin_menu.py
@@ -1,38 +1,51 @@
+from queries.for_appeal import get_all_appeals_query
+
 from .admin_func import season_create, season_delete, season_update
 
-def admin_menu(email: str):
+def admin_menu():
     print("""
 1. Season
 2. Accepting requests
 3. Announcement of winners
-4. All statistics
-5. View results live
-6. Quit
+4. View results live
+5. Quit
     """)
     choice = input("Enter your choice: ")
 
     if choice == '1':
         season_menu()
-
+        return admin_menu()
     elif choice == '2':
-        # remove_manager(email)
-        pass
-
+        accepting_menu()
     elif choice == '3':
-        # show_manager(email)
         pass
-
     elif choice == '4':
-        # edit_manager(email)
         pass
-    elif choice == '6':
+    elif choice == '5':
         print("Backing...")
         return None
-
     else:
         print("Invalid choice. Please try again.")
 
-    return admin_menu(email)
+    return admin_menu()
+
+
+def accepting_menu():
+    get_all_appeals_query()
+    print("""""
+1. Accepting requests by ID
+2. Quit
+""")
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    else:
+        print("Invalid choice. Please try again.")
+       
+    return accepting_menu()
 
 
 def season_menu():
@@ -46,20 +59,14 @@ def season_menu():
 
     if choice == '1':
         season_create()
-        pass
-
     elif choice == '2':
         season_update()
-        pass
-
     elif choice == '3':
         season_delete()
-        pass
-    elif choice == '6':
+    elif choice == '4':
         print("Backing...")
-        return None
-
+        return admin_menu
     else:
         print("Invalid choice. Please try again.")
 
-    return admin_menu()
+    return season_menu()
